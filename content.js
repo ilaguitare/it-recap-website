1120f8cbea1e580dc9f0b1e69fa88f4bf65d226e
Ali Kazem <asainska@gmail.com>
2026-03-04 14:25:51 +0100

    error

--- content.js
+++ content.js
@@ -120,7 +120,7 @@ const content = {
             { "name": { "en": "Hussein Charara", "ar": "حسين شرارة" }, "aff": { "en": "Lebanese University, Lebanon", "ar": "الجامعة اللبنانية، لبنان" }, "img": "images/chairs/hussein-charara.jpg" },
             { "name": { "en": "Charles Lim", "ar": "شارل ليم" }, "aff": { "en": "Geometer in IGN, France", "ar": "مساح في IGN، فرنسا" }, "img": "images/chairs/charles-lim.jpg" },
             { "name": { "en": "Bassam Kurdy", "ar": "بسام كردي" }, "aff": { "en": "Mines ParisTech, France", "ar": "RATP باريس، فرنسا" }, "img": "images/chairs/bassam-kurdy.jpg" },
-            { "name": { "en": "Bassem Alachkar", "ar": "باسم الأشقر" }, "aff": { "en": "Manchester, SKA Observatory, UK", "ar": "مانشيستر, انكلترا" }, "img": "images/chairs/bassem-alachkar.jpg" }
+            { "name": { "en": "Bassem Alachkar", "ar": "باسم الأشقر" }, "aff": { "en": "Manchester, SKA Observatory, UK", "ar": "مانشيستر, انكلترا" }, "img": "images/chairs/bassem-alachkar.jpg" },
             { "name": { "en": "Gregory Lesouble", "ar": "غريغوري لوسوبل" }, "aff": { "en": "RATP Paris, France", "ar": "RATP باريس، فرنسا" }, "img": "images/chairs/gregory-lesouble.jpg" },
             { "name": { "en": "Hamza Chaal", "ar": "حمزة شال" }, "aff": { "en": "Siemens Wind Power, UK", "ar": "سيمينز, انكلترا" }, "img": "images/chairs/hamza_chaal.jpg" }
         ],
