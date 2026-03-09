b7437b48acdb1099a863981b1d6cdaa121b56a3e
Ali Kazem <asainska@gmail.com>
2026-03-09 16:50:51 +0100

    .fr

--- content.js
+++ content.js
@@ -106,7 +106,7 @@ const content = {
         "generalChairs": [
             { "name": { "en": "Basel Solaiman", "ar": "باسل سليمان" }, "aff": { "en": "IMT Atlantique, France", "ar": "IMT Atlantique، فرنسا" }, "img": "images/chairs/basel-solaiman.jpg" },
             { "name": { "en": "Mohamed-Ali Alsayed-Ali", "ar": "محمد علي السيد علي" }, "aff": { "en": "HIAST, Syria", "ar": "HIAST، سوريا" }, "img": "images/chairs/mohamed-ali-alsayed.jpg" },
-            { "name": { "en": "Ali Kazem", "ar": "علي كاظم" }, "aff": { "en": "HIAST, Syria", "ar": "HIAST، سوريا" }, "img": "images/chairs/ali-kazem.jpg" },
+            { "name": { "en": "Ali Kazem", "ar": "علي كاظم" }, "aff": { "en": "ali.kazem.fr, HIAST, Syria", "ar": "HIAST، سوريا" }, "img": "images/chairs/ali-kazem.jpg" },
             { "name": { "en": "Amer Baghdadi", "ar": "عامر بغدادي" }, "aff": { "en": "Institut Mines-Télécom, France", "ar": "معهد مين تيليكوم، فرنسا" }, "img": "images/chairs/amer-baghdadi.jpg" }
         ],
         
